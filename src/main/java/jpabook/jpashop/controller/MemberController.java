446675ae6b9cba47f4ac6f5208595f3032e52ba7
jaekook <jaekook4849@gmail.com>
2021-02-20 02:48:27 +0900

    Add Member Form and Enroll

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping(value = "/members/new")     // URL을 열어보는 역할
+    public String createForm(Model model) {
+        // Controller에서 View로 넘어갈 때, 아래 데이터를 실어서 넘긴다.
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")    // 데이터를 등록하는 역할
+    public String create(@Valid MemberForm form, BindingResult result) {
+        /**
+         * @Valid 애노테이션으로 MemberForm 클래스의 @NotEmpty 필드가 비어있으면 안된다.
+         * BindingResult 클래스에 오류를 담는다.
+         */
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+}
